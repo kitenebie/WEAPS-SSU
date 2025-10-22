114a0cdec6c553364126673ea77ae2c91327ddc0
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-23 00:11:21 +0800

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,8 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\AdminRoleMiddleware;
 use Jeffgreco13\FilamentBreezy\BreezyCore;
 use Resma\FilamentAwinTheme\FilamentAwinTheme;
 use Leandrocfe\FilamentApexCharts\FilamentApexChartsPlugin;
@@ -91,6 +93,7 @@ class AdminPanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+                AdminRoleMiddleware::class,
             ])
             ->authMiddleware([
                 Authenticate::class,
