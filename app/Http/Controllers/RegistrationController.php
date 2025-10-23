b2c3071752c24f47d87ac5e48880ee5d9d9e75ab
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-23 08:06:21 +0800

    klcs

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -57,9 +57,9 @@ class RegistrationController extends Controller
     public function showCompanyVerificationStatus()
     {
         $userCampany = Company::where('user_handle', Auth::id())->first();
-        return $userCampany;
-        if (!Auth::check() || !$userCampany || $userCampany->isActive) {
-            return redirect('/employeer/company-verification-status');
+        // return $userCampany;
+        if (!Auth::check() || !$userCampany || !$userCampany->isActive) {
+            return redirect()->back();
         }
         return view('filament.employeer.pages.company-verification-status');
     }
