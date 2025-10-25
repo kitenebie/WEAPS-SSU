489e2681c91dfa761035ed2fbd85c8ab535b1269
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-25 15:50:43 +0800

    ds

--- app/Filament/Widgets/DashboardStatsWidget.php
+++ app/Filament/Widgets/DashboardStatsWidget.php
@@ -19,7 +19,7 @@ class DashboardStatsWidget extends BaseWidget
             Grid::make()
                 ->columns([
                     'sm' => 1,
-                    'md' => 2,
+                    'md' => 1,
                     'lg' => 4,
                 ])->columnSpanFull()
                 ->schema([
