da397587dc983fd8b421aba9ebbb25c79b3d4395
Codego <101083976+kitenebie@users.noreply.github.com>
2025-10-28 10:11:22 +0800

    ds

--- app/Filament/Employeer/Pages/Auth/Register.php
+++ app/Filament/Employeer/Pages/Auth/Register.php
@@ -129,6 +129,7 @@ class Register extends BaseRegister
                             <button style="Display: flex !important" wire:click="$set(\'registerMode\', \'normal\')" class="btn-signup mb-4 fi-color fi-color-primary fi-bg-color-600 hover:fi-bg-color-500 dark:fi-bg-color-600 dark:hover:fi-bg-color-500 fi-text-color-0 hover:fi-text-color-0 dark:fi-text-color-0 dark:hover:fi-text-color-0 fi-btn fi-size-md  fi-ac-btn-action" type="button">
                         Try another way
                     </button>
+                        <style>
                         button:nth-child(1)
                         {
                             display: none !important;
