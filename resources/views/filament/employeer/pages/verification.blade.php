0b6a8b38254f743279ef22be563235e11823da41
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-18 07:34:55 +0800

    csddc

--- resources/views/filament/employeer/pages/verification.blade.php
+++ resources/views/filament/employeer/pages/verification.blade.php
@@ -105,7 +105,7 @@
                     <div class="text-center">
                         <!-- Applicant Icon -->
                         <div class="inline-flex items-center justify-center w-16 h-16 bg-red-50 rounded-full mb-6">
-                            <svg class="w-8 h-8 text-red-800" fill="none" stroke="currentColor"
+                            <svg class="w-8 h-8 text-red-800" fill="#FEF6F6" stroke="currentColor"
                                 viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
