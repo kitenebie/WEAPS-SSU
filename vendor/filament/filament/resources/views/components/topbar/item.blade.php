982bf5dcb379d8d2f3f3a4537e9437b52eb08e1a
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-19 10:00:59 +0800

    dsd

--- vendor/filament/filament/resources/views/components/topbar/item.blade.php
+++ vendor/filament/filament/resources/views/components/topbar/item.blade.php
@@ -13,20 +13,14 @@
     $tag = $url ? 'a' : 'button';
 @endphp
 
-<li @class([
-    'fi-topbar-item',
-    'fi-active' => $active,
-])>
+<li @class(['fi-topbar-item', 'fi-active' => $active])>
     <{{ $tag }}
-        @if ($url)
-            {{ \Filament\Support\generate_href_html($url, $shouldOpenUrlInNewTab) }}
+        @if ($url) {{ \Filament\Support\generate_href_html($url, $shouldOpenUrlInNewTab) }}
         @else
-            type="button"
-        @endif
-        class="fi-topbar-item-btn"
-    >
+            type="button" @endif
+        class="fi-topbar-item-btn">
         @if ($icon || $activeIcon)
-            {{ \Filament\Support\generate_icon_html(($active && $activeIcon) ? $activeIcon : $icon, attributes: (new \Illuminate\View\ComponentAttributeBag)->class(['fi-topbar-item-icon'])) }}
+            {{ \Filament\Support\generate_icon_html($active && $activeIcon ? $activeIcon : $icon, attributes: new \Illuminate\View\ComponentAttributeBag()->class(['fi-topbar-item-icon'])) }}
         @endif
 
         <span class="fi-topbar-item-label">
@@ -34,17 +28,20 @@
         </span>
 
         @if (filled($badge))
-            <x-filament::badge
-                :color="$badgeColor"
-                size="sm"
-                :tooltip="$badgeTooltip"
-            >
+            <x-filament::badge :color="$badgeColor" size="sm" :tooltip="$badgeTooltip">
                 {{ $badge }}
             </x-filament::badge>
         @endif
 
-        @if (! $url)
-            {{ \Filament\Support\generate_icon_html(\Filament\Support\Icons\Heroicon::ChevronDown, alias: \Filament\View\PanelsIconAlias::TOPBAR_GROUP_TOGGLE_BUTTON, attributes: (new \Illuminate\View\ComponentAttributeBag)->class(['fi-topbar-group-toggle-icon'])) }}
+        @if (!$url)
+            {{ \Filament\Support\generate_icon_html(\Filament\Support\Icons\Heroicon::ChevronDown, alias: \Filament\View\PanelsIconAlias::TOPBAR_GROUP_TOGGLE_BUTTON, attributes: new \Illuminate\View\ComponentAttributeBag()->class(['fi-topbar-group-toggle-icon'])) }}
         @endif
-    </{{ $tag }}>
+        </{{ $tag }}>
+        <style>
+            .fi-topbar {
+                background: #7F1D1D !important;
+                border-bottom-color: #FFB900 !important;
+                border-bottom-width: 2px !important;
+            }
+        </style>
 </li>
