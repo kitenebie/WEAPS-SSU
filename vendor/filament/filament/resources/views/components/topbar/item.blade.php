f93b022c5aa4131fadf10b90634b78fc0f50c94c
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-19 10:04:07 +0800

    dfhsh

--- vendor/filament/filament/resources/views/components/topbar/item.blade.php
+++ vendor/filament/filament/resources/views/components/topbar/item.blade.php
@@ -44,19 +44,17 @@
                 border-bottom-width: 2px !important;
             }
 
-            .fi-topbar-item .fi-active.fi-topbar-item-btn {
+            /* active item */
+            .fi-topbar-item.fi-active .fi-topbar-item-btn {
                 background: #FFB900 !important;
                 color: #7F1D1D !important;
             }
 
-            .fi-topbar-item .fi-active .fi-topbar-item-btn {
-                background: #FFB900 !important;
-                color: #7F1D1D !important;
-            }
-
+            /* hover */
             .fi-topbar-item-btn:hover {
                 background: #FECF55 !important;
                 color: #7F1D1D !important;
             }
         </style>
+
 </li>
