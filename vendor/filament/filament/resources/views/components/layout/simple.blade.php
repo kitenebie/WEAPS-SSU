712fc3eb87002cd7fb9c7a262a79feed6e26230e
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-21 07:25:49 +0800

    update

--- vendor/filament/filament/resources/views/components/layout/simple.blade.php
+++ vendor/filament/filament/resources/views/components/layout/simple.blade.php
@@ -4,7 +4,7 @@
     $livewire ??= null;
 
     $renderHookScopes = $livewire?->getRenderHookScopes();
-    $maxContentWidth ??= (filament()->getSimplePageMaxContentWidth() ?? Width::Large);
+    $maxContentWidth ??= filament()->getSimplePageMaxContentWidth() ?? Width::Large;
 
     if (is_string($maxContentWidth)) {
         $maxContentWidth = Width::tryFrom($maxContentWidth) ?? $maxContentWidth;
@@ -36,12 +36,12 @@
         @endif
 
         <div class="fi-simple-main-ctn">
-            <main
-                @class([
-                    'fi-simple-main',
-                    ($maxContentWidth instanceof Width) ? "fi-width-{$maxContentWidth->value}" : $maxContentWidth,
-                ])
-            >
+            <main @class([
+                'fi-simple-main',
+                $maxContentWidth instanceof Width
+                    ? "fi-width-{$maxContentWidth->value}"
+                    : $maxContentWidth,
+            ])>
                 {{ $slot }}
             </main>
         </div>
@@ -51,15 +51,36 @@
         {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::SIMPLE_LAYOUT_END, scopes: $renderHookScopes) }}
     </div>
     <style>
-        .fi-simple-layout{
-            background-color: #7F1D1D !important;
+        .fi-simple-layout {
+            /* background-color: #7F1D1D !important; */
         }
-        .fi-ac-btn-action{
+
+        .fi-ac-btn-action {
             background-color: #7F1D1D !important;
-            color: #1E1E1E !important;
+            color: #ffffff !important;
         }
-        .fi-link{
+
+        .fi-link {
             color: #7F1D1D !important;
+        }
+
+        .fi-simple-main-ctn {
+            flex-grow: 1;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            display: flex;
+            position: relative;
+            overflow: hidden;
+            background: linear-gradient(135deg, rgba(109, 1, 1, 0.83) 0%, rgba(128, 0, 0, 0.7) 30%, rgba(100, 1, 1, 0.8) 70%, rgba(92, 1, 1, 0.842) 100%), url(/bg.png);
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            backdrop-filter: blur(20px) saturate(180%);
+            -webkit-backdrop-filter: blur(20px) saturate(180%);
+            border: 1px solid rgba(255, 255, 255, 0.3);
+            box-shadow: 0 8px 32px rgba(0, 0, 0, 0.1), 0 4px 16px rgba(0, 0, 0, 0.05), inset 0 1px 0 rgba(255, 255, 255, 0.4), inset 0 -1px 0 rgba(255, 255, 255, 0.2);
         }
     </style>
 </x-filament-panels::layout.base>
