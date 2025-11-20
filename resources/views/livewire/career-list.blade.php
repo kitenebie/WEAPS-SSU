20db64e4dbf6e616628e7a0762f0bcfc0f9ce2c0
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-20 17:07:40 +0800

    ksds

--- resources/views/livewire/career-list.blade.php
+++ resources/views/livewire/career-list.blade.php
@@ -257,7 +257,7 @@
                             @endif
 
                             @if ($career->min_salary || $career->max_salary)
-                                <div class="flex px-4 py-2 items-center text-xl font-bolder mb-2">
+                                <div class="flex px-4 py-2 items-center text-xl font-bold mb-2">
                                     {{-- <svg class="w-4 h-4 mr-2 text-amber-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1"></path>
                                 </svg> --}}
