cbf3c5abad993438174c83979b71bac63f7c28d5
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-26 15:27:46 +0800

    ds

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,16 +54,16 @@ class AdminPanelProvider extends PanelProvider
                 ])
             ->darkMode(false)
             ->plugins([
-                BreezyCore::make()
-                    ->myProfile(
-                        shouldRegisterUserMenu: true, // Sets the 'account' link in the panel User Menu (default = true)
-                        userMenuLabel: 'My Profile', // Customizes the 'account' link label in the panel User Menu (default = null)
-                        shouldRegisterNavigation: false, // Adds a main navigation item for the My Profile page (default = false)
-                        navigationGroup: 'Settings', // Sets the navigation group for the My Profile page (default = null)
-                        hasAvatars: false, // Enables the avatar upload form component (default = false)
-                        slug: 'my-profile' // Sets the slug for the profile page (default = 'my-profile')
-                    )
-                    ->enableTwoFactorAuthentication(),
+                // BreezyCore::make()
+                //     ->myProfile(
+                //         shouldRegisterUserMenu: true, // Sets the 'account' link in the panel User Menu (default = true)
+                //         userMenuLabel: 'My Profile', // Customizes the 'account' link label in the panel User Menu (default = null)
+                //         shouldRegisterNavigation: false, // Adds a main navigation item for the My Profile page (default = false)
+                //         navigationGroup: 'Settings', // Sets the navigation group for the My Profile page (default = null)
+                //         hasAvatars: false, // Enables the avatar upload form component (default = false)
+                //         slug: 'my-profile' // Sets the slug for the profile page (default = 'my-profile')
+                //     )
+                //     ->enableTwoFactorAuthentication(),
                 // FilamentAwinTheme::make(),
                 FilamentApexChartsPlugin::make(),
                 FilamentShieldPlugin::make(), // Registers RoleResource (Spatie Shield)
