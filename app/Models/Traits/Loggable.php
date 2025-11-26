60620e7e0a4b508ce9ef7a922d97c84b7f4a4dc0
Codego <101083976+kitenebie@users.noreply.github.com>
2025-11-26 09:31:56 +0800

    update

--- app/Models/Traits/Loggable.php
+++ app/Models/Traits/Loggable.php
@@ -8,6 +8,19 @@ use Illuminate\Support\Facades\Request;
 
 trait Loggable
 {
+    protected static function logChange($model, $action, $changes = null)
+    {
+        SystemLog::create([
+            'model_type' => get_class($model),
+            'model_id' => $model->getKey(),
+            'action' => $action,
+            'changes' => $changes,
+            'user_id' => Auth::id(),
+            'ip_address' => Request::ip(),
+            'user_agent' => Request::userAgent(),
+        ]);
+    }
+
  public static function bootLogsActivity()
     {
         static::created(function ($model) {
